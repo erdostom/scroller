2bec8b93c962487eb7670dae40d143e770151950
Tamas Erdos <tamas@tamaserdos.com>
2014-05-02 17:04:12 -0400

    all banners added to scroller

--- js/scrolldetect.js
+++ js/scrolldetect.js
@@ -1,31 +1,32 @@
 'use strict';
 var SYK = {
     playing_animations: [],
+    $animations: $('.rectangle'),
     updateScroll: function() {
         SYK.updateScrollVariables();
         SYK.logNewItems();
     },
     updateScrollVariables: function() {
-        SYK.scrollTop = $(window).scrollTop()
-        SYK.scrollBottom = SYK.scrollTop + $(window).height()
+        SYK.scrollTop = $(window).scrollTop();
+        SYK.scrollBottom = SYK.scrollTop + $(window).height();
     },
     itemInViewport: function($item) {
-        return $item.offset().top < SYK.scrollBottom
+        return $item.offset().top < SYK.scrollBottom;
     },
     itemNotPlayedYet: function($item) {
-        return $.inArray($item.attr('id'), SYK.playing_animations) == -1
+        return $.inArray($item.attr('id'), SYK.playing_animations) == -1;
     },
     shouldPlayItem: function($item) {
-        return SYK.itemInViewport($item) && SYK.itemNotPlayedYet($item)
+        return SYK.itemInViewport($item) && SYK.itemNotPlayedYet($item);
     },
     playItem: function($item) {
-        console.log("now playing #" + $item.attr('id'))
+        console.log('now playing #' + $item.attr('id'));
     },
     logNewItems: function() {
-        $('.rectangle').each(function() {
+        SYK.$animations.each(function() {
             var that = $(this);
             if (SYK.shouldPlayItem(that)) {
-                SYK.playing_animations.push(that.attr('id'))
+                SYK.playing_animations.push(that.attr('id'));
                 SYK.playItem(that);
             }
         });
@@ -35,5 +36,5 @@ $(function() {
     SYK.updateScroll();
     $(window).scroll(function() {
         SYK.updateScroll();
-    })
+    });
 });
